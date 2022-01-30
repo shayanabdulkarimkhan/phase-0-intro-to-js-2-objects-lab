82c424931440095dd57c0bb7114e4c5f918d65ef
shayan <sak704@nyu.edu>
2022-01-30 11:38:21 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const employee= {
+    name: "Shayan",
+    streetAddress: "6851 Fessenden"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    const newEmployee = {...object};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+}
+
+function deleteFromEmployeeByKey(object, key){
+    const newEmployee = {...object};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+}
+
+
 // Write your solution in this file!
